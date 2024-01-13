72e56253324a8142349effb2449fcf72ec01dc10
Eduardo Lopez <eduardo.o.lopez85@gmail.com>
2024-01-12 22:42:25 -0300

    complete day 6

---
+++ advientoDay6.js
@@ -0,0 +1,12 @@
+// Objects
+
+const myObject = {
+    name: "Javascript",
+    year: 2023,
+    isValid: true
+}
+
+console.log(myObject)
+console.log(myObject.name)
+// we can use array notation to access properties, but consider to pass the key in string format
+console.log(myObject["year"])
