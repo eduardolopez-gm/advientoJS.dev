cd26ce91663f3372805e69d93aa75fbe71c1a3ad
Eduardo Lopez <eduardo.o.lopez85@gmail.com>
2024-01-15 16:12:39 -0300

    complete day16

---
+++ advientoDay16.js
@@ -0,0 +1,9 @@
+// Array filter
+const numbers = [1, 2, 3, 4, 5, 6]
+
+const filternumbers = numbers.filter( number => number > 3)
+
+console.log("Original array")
+console.log(numbers)
+console.log("Filter array")
+console.log(filternumbers)
