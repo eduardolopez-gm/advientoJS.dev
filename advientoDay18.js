5ae7489f42a6dedfd3e3d7af442d77e62d2c7f9d
Eduardo Lopez <eduardo.o.lopez85@gmail.com>
2024-01-15 16:29:33 -0300

    complete day 18

---
+++ advientoDay18.js
@@ -0,0 +1,10 @@
+// Nullish Coalescing
+// ??
+// check if a value exist(not null) if not apply a default value
+
+
+const input = null
+const defaultValue = "default value for this field"
+
+console.log(input??defaultValue)
+
